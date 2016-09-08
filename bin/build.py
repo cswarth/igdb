734941f860c0905fe702c72d2eb2e628b99dd1a8
Chris Warth <cwarth@fhcrc.org>
2016-09-08 15:43:48 -0700

    download fasta files
    * added Gene class for storing everything we know about one gene
    * create fasta download files.
    * link fasta file download in gene pages.

--- bin/build.py
+++ bin/build.py
@@ -6,14 +6,6 @@
 
 from __future__ import print_function
 
-from bokeh.embed import components
-from bokeh.resources import CDN
-from bokeh.util.browser import view
-from bokeh.models import Legend
-from bokeh.plotting import figure, ColumnDataSource
-from bokeh.models import HoverTool
-from bokeh.models import NumeralTickFormatter
-from bokeh.palettes import Spectral11
 
 import os
 import sys
@@ -22,61 +14,104 @@ import argparse
 import datetime
 import getpass
 import hashlib
-import collections
 import pandas as pd
 from cStringIO import StringIO
 from jinja2 import Environment, FileSystemLoader
 
-from collections import OrderedDict
-from collections import defaultdict
+from Bio.Seq import Seq
+from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
+from Bio.Alphabet import IUPAC
 
-genes = defaultdict(dict)
+# global options dict for command line arguments
+options = dict()
 
-class Gene(defaultdict):
-    def __init__(self, name=None):
-        super(Gene, self).__init__(self, dict)
-
-    def load():
-        pass
-    
-    def render():
-        pass
-
-
-# convert a dict to an appropriate gene page.
-def render_page(filename):
-    env = Environment(loader=FileSystemLoader(THIS_DIR),
+def load_template(name):
+    # Capture parent directory above where this script lives.  
+    parent = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+    templatedir = os.path.join(parent, 'templates')
+    env = Environment(loader=FileSystemLoader(templatedir),
                           trim_blocks=True)
     # Alias str.format to strformat in template
     env.filters['strformat'] = str.format
-    template = env.get_template("templates/gene.jinja")
+    template = env.get_template(name)
+    return template
 
-    renderdict = {
-        'date':datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-        'command':" ".join(sys.argv),
-        'workdir':os.getcwd(),
-        'user':getpass.getuser(),
-        'title':"IgDB"
+class Gene(object):
+    template = load_template('gene.jinja')
+    
+    @classmethod
+    def fromfile(cls, filename):
+        gene = None
+        with open(filename, 'rb') as fp:
+            data = json.load(fp)
+            if type(data) is dict:
+                gene = cls(data)
+        return gene
+    
+    def __init__(self, data):
+        super(Gene, self).__init__()
+        self.data = data
+
+
+    # Write a FASTA file containing the gene id and nucleotide sequence.
+    # If `options.dryrun` is TRUE, don't actually write the file, but do everything else.
+    #
+    # return: string name of the fasta file.
+    def render_fasta(self):
+        print(self.data['id'])
+
+        def to_fasta(seq):
+            fp = StringIO()
+            record = SeqRecord(Seq(seq, IUPAC.extended_dna),
+                                   id=self.data['id'])
+            SeqIO.write(record, fp,
+                    'fasta')
+            return fp.getvalue()
+
+        
+        fasta = to_fasta(self.data['sequence'])
+        
+        digest = hashlib.sha1(fasta).hexdigest()
+        print('fasta -> {}'.format(digest))
+        outname = os.path.join(options.output, digest)
+        if not options.dryrun:
+            with open(outname, "w") as f:
+                f.write(fasta)
+
+        self.data['fastafile'] = digest
+        return outname
+        
+
+    # Render this gene through jinja template to produce HTML file.
+    # If `options.dryrun` is TRUE, don't actually write the file, but do everything else.
+    #
+    # return: string name of the html file.
+    def render_html(self):
+        renderdict = {
+            'date': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            'command': " ".join(sys.argv),
+            'workdir': os.getcwd(),
+            'user': getpass.getuser(),
+            'title': 'IgDB',
         }
+                
+        renderdict.update(self.data)
+        page = Gene.template.render(**renderdict) 
+        html = page.encode('utf-8')
 
-    page = None
-    with open(filename, 'rb') as fp:
-        obj = json.load(fp)
-        if type(obj) is dict:
-            renderdict.update(obj)
-            page = template.render(**renderdict) 
-            
-    return page
-
-    # to deploy to gh-pages, use the function `gh=deploy` defined in ~.bash_profile
-    # first copy index.html tp the `site/` directory
-    # Cribbed from https://gist.github.com/cobyism/4730490
-    #    git subtree push --prefix site origin gh-pages
-    # if a.view:
-    #     view(a.outfile)
+        # write the html to a file named with the sha1 hash of its contents.
+        digest = hashlib.sha1(html).hexdigest()
+        print('{} -> {}'.format(self.data['id'], digest))
+        outname = os.path.join(options.output, digest)
+        if not options.dryrun:
+            with open(outname, "w") as f:
+                f.write(html)
+        self.htmlfile = outname
+        return outname
 
 
-# Generate the names of all json gene files under `dir`, yielding each
+# Generate the names of all json input files under `dir`, yielding each
 # one in turn until there are no more files left to process.
 def content_file_iterator(dir):
     for root, dirs, files in os.walk(dir):
@@ -85,22 +120,24 @@ def content_file_iterator(dir):
                 yield os.path.join(root, f)
 
 
-# Capture parent directory above 'templates/'
-THIS_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
 
 def main():
     p = argparse.ArgumentParser()
     p.add_argument('-t', '--template', default='template.jinja',
             help="""Jinja2 Tempate file[default: %(default)]""")
     p.add_argument('-c', '--content', default="content",
-            help="""Directory where content can be found (relative of absolute): %(default)]""")
+            help="""Directory where content can be found: %(default)]""")
     p.add_argument('-o', '--output', default="output",
             help="""Directory where output should be left: %(default)]""")
     p.add_argument('-n', '--dryrun', action='store_true',
             help="""dry run""")
     p.add_argument('-v', '--verbose', default=False,
             help="""enable verbose output""")
-    a = p.parse_args()
+
+    global options
+    print(type(options))
+    options = p.parse_args()
+    print(type(options))
 
     # read in JSON file
 
@@ -112,16 +149,11 @@ def main():
     # Each has the name, sequence, evidence,and dowload links
     # produce the downloadable content
     # filterable?
-    for f in content_file_iterator(a.content):
-        page = render_page(f)
-        if page is not None:
-            html = page.encode('utf-8')
-            digest = hashlib.sha1(html).hexdigest()
-            print('{} -> {}'.format(f, digest))
-            if not a.dryrun:
-                with open(os.path.join(a.output, digest), "w") as f:
-                    f.write(html)
-                    
+    for (i,f) in enumerate(content_file_iterator(options.content)):
+        g = Gene.fromfile(f)
+        if g is not None:
+            g.render_fasta()
+            g.render_html()
 
     
     # idx = pd.read_csv(os.path.join(a.content, 'index.csv'))
